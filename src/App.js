473e8798a20376331bc4ccbfef8a3719fe37b448
asima-chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2020-11-20 02:13:07 +0600

    home section, about section, service section completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import HomeMain from './components/HomeMain/HomeMain';
+import About from './components/About/About';
+import Services from './components/Services/Services';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HomeMain />
+      <About />
+      <Services />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo from '../../siteImages/logo.png';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container">
+                <a className="navbar-brand" href="#home">
+                    <img src={logo} alt="logo" className="logo" />
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active mx-2">
+                            <a className="nav-link" href="#home">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item mx-2">
+                            <a className="nav-link" href="#about">About</a>
+                        </li>
+                        <li className="nav-item mx-2">
+                            <a className="nav-link" href="#services">Services</a>
+                        </li>
+                        <li className="nav-item mx-2">
+                            <a className="nav-link" href="#pricing">Pricing</a>
+                        </li>
+                        <li className="nav-item mx-2">
+                            <a className="nav-link" href="#">Our Team</a>
+                        </li>
+                        <li className="nav-item mx-2">
+                            <a href="#contact">
+                                <button type="button" className="btn main-button">Contact us</button>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
